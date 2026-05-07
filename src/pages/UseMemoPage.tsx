ae32b59d87e036ab5fb0eb55287e3efff179c540
Margulan Zhumabek <dxskull9@gmail.com>
2026-05-07 12:19:58 +0500

    Add bad page and modify useMemo page

--- src/pages/UseMemoPage.tsx
+++ src/pages/UseMemoPage.tsx
@@ -1,11 +1,9 @@
-
 const UseMemoPage = () => {
-    console.log("useMemo")
-    return (
-        <div>
-            <h1>useMemo</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>useMemo</h1>
+    </div>
+  );
 };
 
 export default UseMemoPage;
