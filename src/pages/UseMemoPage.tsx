f45454b0e16e1dabf06f4207f52c3dba5b41a9a1
Margulan Zhumabek <dxskull9@gmail.com>
2026-05-07 12:24:53 +0500

    Remove bad page and modify useMemo page

--- src/pages/UseMemoPage.tsx
+++ src/pages/UseMemoPage.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
 const UseMemoPage = () => {
-    return (
-        <div>
-            <h1>useMemo page</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>useMemo</h1>
+    </div>
+  );
 };
 
 export default UseMemoPage;
